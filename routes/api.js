a5dc951d113c17f81b5aeaf92d1be7e5b44b8efc
johnvwood <jwood112035@fanshaweonline.ca>
2021-03-11 17:19:25 -0500

    movie id from path

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 //import express
-const express = require('express');
+const express = require("express");
+const { createPool } = require("mysql");
 const router = express.Router();
 
 //import sql
-const connect = require('../config/sqlConfig')
+const connect = require("../config/sqlConfig")
 
 router.get("/", (req, res) => {
     //json encode in java
@@ -18,9 +19,10 @@ router.get("/movies", (req, res) => {
     //mysql documentation, select call
     connect.getConnection((err, connection) => {
         if (err) throw err; //no connection error
-        connection.query('SELECT * FROM tbl_movies', (error, results) => {
+        connection.query(`SELECT * FROM tbl_movies`, (error, results) => {
             connection.release();
             if (error) throw error; //post-release error
+
             res.json(results);
         });
     });
@@ -29,7 +31,13 @@ router.get("/movies", (req, res) => {
 //dynamic, can accept a parameter
 //currently accessing the 'id' param in 'movies'
 router.get("/movies/:id", (req, res) => {
-    res.json({ message: "all movies route", movie: req.params.id });
+    connect.query(`SELECT * FROM tbl_movies WHERE movies_id=${req.params.id}`, (error, results) => {
+        if (error) throw error; //no connection error
+        console.log("results:", results); // can add fields for additional console data
+
+        res.json(results);
+    });
+    
 });
 
 module.exports = router;
