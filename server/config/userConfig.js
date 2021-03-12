faaaafbaadb1bef4b059dc6a5128669895a739a3
johnvwood <j_wood112035@fanshaweonline.ca>
2021-03-12 14:06:55 -0500

    added old branch files

---
+++ server/config/userConfig.js
@@ -0,0 +1,10 @@
+// SQL connect info
+const sqlCreds = {
+    user     : "root",
+    password : "",
+    host     : "localhost",
+    port     : 3306,
+    database : "roku_streaming"
+}
+
+module.exports = sqlCreds;
